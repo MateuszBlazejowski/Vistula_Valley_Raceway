acf77354d78ae28e956a554d6ea70fcd61780e4b
MateuszBlazejowski <matblaz12345@gmail.com>
2024-11-25 22:00:14 +0100

    UpdateVisuals

--- VVR/Visuals/ImageGenerating.cs
+++ VVR/Visuals/ImageGenerating.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+//using System.Threading;
 using System.Threading.Tasks;
 using VVR.Vehicles; 
 
 public static class GlobalConstants
 {
-    public const int MaxTrackWidth = 100;
+    public const int MAXTRACKWIDTH = 100;
+    public const int TRACKFRAMELENGTH = 15;
 }
 
 namespace VVR.Visuals
@@ -17,13 +19,15 @@ namespace VVR.Visuals
     {
         //will store all cars in a container independetnly to logic, that will allow to generate everything separately
         //events will modify cars properties like speed, position(turning left and right on track) etc, which will influence visuals 
+  
+
         public ImageGenerating()
         {
 
         }
 
         public List<TrackPiece> trackPieces = new List<TrackPiece>();
-        public List<Vehicle> vehicles = new List<Vehicle>(); 
+        public List<Vehicle> vehicles = new List<Vehicle>();
         public void GenerateFixedTrack() //for now track is fixed, but in future it is possible to write a function that will generate random track 
         {
             trackPieces.Clear();
@@ -57,10 +61,39 @@ namespace VVR.Visuals
             trackPieces.Add(new TrackPiece(0, 50, '|', '|')); // the track is very short but sufficient for testing //FOR FURTHER DEVELOPMENT 
         }
 
+        public char[,] RenderTrackFrame(int startingRow)
+        {
+            char[,] toReturn = new char[GlobalConstants.MAXTRACKWIDTH, GlobalConstants.TRACKFRAMELENGTH];
+            for (int i = 0; i < GlobalConstants.MAXTRACKWIDTH; i++)
+            {
+                for (int j = 0; j < GlobalConstants.TRACKFRAMELENGTH; j++)
+                {
+                    toReturn[i, j] = ' ';
+                }
+            }
 
+            for (int i = 0; i < GlobalConstants.TRACKFRAMELENGTH; i++)
+            {
+                toReturn[((startingRow + i) % trackPieces.Count), i] = ' ';
+                //trackPieces[((startingRow+i)%trackPieces.Count)
+            }
+            return toReturn; 
+        }   
         public void Play()
         {
+            int startingRow = 0; 
+            char[,] frame = new char[GlobalConstants.MAXTRACKWIDTH, GlobalConstants.TRACKFRAMELENGTH];
+            while (true)
+            {
+                frame = RenderTrackFrame(startingRow); 
 
+                // later add objects to the track like cars etc 
+                startingRow++;
+                startingRow = startingRow % trackPieces.Count;
+
+                //stop condition, some event like pressing the esc button 
+                Thread.Sleep(500);  //later depending on your speed  
+            }
             //will independently generate all visuals, respond to events which will change the the values of some things, that will influence display 
             // 
         }
